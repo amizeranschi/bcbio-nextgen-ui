d38d19bd4030562c25716db18b9e3995349491ec
amizeranschi <alex.mizeranschi@gmail.com>
2022-08-13 20:47:26 +0300

    Update setup_python3_env.sh
    Install more conda packages in extra3

--- bcbio_wrapper_scripts/environment/setup_python3_env.sh
+++ bcbio_wrapper_scripts/environment/setup_python3_env.sh
@@ -17,10 +17,13 @@ if [[ ! -d ${bcbio_install_path}/extra3 ]]; then
    ln -s ${bcbio_install_path}/extra3/bin/mamba ${bcbio_install_path}/extra3/bin/mamba_extra3
 
    ## installing packages
-   ${bcbio_install_path}/extra3/bin/mamba_extra3 install --yes -c conda-forge -c bioconda wget git bedops vcftools sra-tools pyyaml perl entrez-direct tassel beagle plink openssl=1.1.1l ensembl-vep=105 "bcftools>=1.13" biobambam=2.0.87
-   ${bcbio_install_path}/extra3/bin/mamba_extra3 install --yes -c conda-forge r-base
-   ${bcbio_install_path}/extra3/bin/mamba_extra3 install --yes -c conda-forge r-essentials
-   ${bcbio_install_path}/extra3/bin/conda_extra3 install --yes -c conda-forge gxx_linux-64
+#   ${bcbio_install_path}/extra3/bin/mamba_extra3 install --yes -c conda-forge -c bioconda wget git bedops vcftools sra-tools pyyaml perl entrez-direct tassel beagle plink openssl=1.1.1l ensembl-vep=105 "bcftools>=1.13" biobambam=2.0.87
+#   ${bcbio_install_path}/extra3/bin/mamba_extra3 install --yes -c conda-forge r-base
+#   ${bcbio_install_path}/extra3/bin/mamba_extra3 install --yes -c conda-forge r-essentials
+#   ${bcbio_install_path}/extra3/bin/mamba_extra3 install --yes -c conda-forge gxx_linux-64
 
-   ${bcbio_install_path}/extra3/bin/conda_extra3 install --yes -c conda-forge -c bioconda bioconductor-annotationhub bioconductor-annotationdbi bioconductor-meshdbi r-ggplot2 bioconductor-clusterprofiler bioconductor-dose bioconductor-meshes bioconductor-chipseeker bioconductor-gosemsim bioconductor-reactomepa bioconductor-variantannotation bioconductor-deseq2 r-rcolorbrewer r-pheatmap bioconductor-enrichplot
+#   ${bcbio_install_path}/extra3/bin/mamba_extra3 install --yes -c conda-forge -c bioconda bioconductor-annotationhub bioconductor-annotationdbi bioconductor-meshdbi r-ggplot2 bioconductor-clusterprofiler bioconductor-dose bioconductor-meshes bioconductor-chipseeker bioconductor-gosemsim bioconductor-reactomepa bioconductor-variantannotation bioconductor-deseq2 r-rcolorbrewer r-pheatmap bioconductor-enrichplot
+   
+   ${bcbio_install_path}/extra3/bin/mamba_extra3 install --yes -c conda-forge -c bioconda gxx_linux-64 wget git bedops vcftools pyyaml sra-tools perl-net-ssleay entrez-direct tassel beagle gcta "gdal>=2.0.1" plink "bcftools>=1.15" haploview gatk4 "ensembl-vep>=105" "perl-bioperl>=1.7.2" "r-base>=4.1" r-essentials "r-raster>=3.5_21" r-xml r-knitr r-rmarkdown r-shiny r-rcpp r-devtools r-reticulate perl-bio-tools-run-alignment-tcoffee r-terra r-rgdal r-ragg bioconductor-annotationhub bioconductor-annotationdbi bioconductor-meshdbi r-ggplot2 bioconductor-clusterprofiler bioconductor-dose bioconductor-meshes bioconductor-chipseeker bioconductor-gosemsim bioconductor-reactomepa bioconductor-variantannotation bioconductor-deseq2 r-rcolorbrewer r-pheatmap bioconductor-enrichplot
+   
 fi
