41977c3999ba4889270a2a1d3ec85cc21e60dd79
amizeranschi <alex.mizeranschi@gmail.com>
2022-09-09 18:59:04 +0300

    Update computeMetrics.R

--- bcbio_wrapper_scripts/downstreamAnalysis/computeMetrics.R
+++ bcbio_wrapper_scripts/downstreamAnalysis/computeMetrics.R
@@ -11,13 +11,11 @@ sgnfGenes_set2 = args[2]
 my_species = args[3]
 workflow_name = args[4]
 
-print(paste0(" --- current species is: ", my_species))
-
 # get database for the species using my_species to search AnnotationHub
 ah_species = AnnotationHub()
 
 ## search for a suitable OrgDb
-qu = query(ah_species, c("OrgDb", my_species))
+qu = query(ah_species, c("OrgDb", sub("_", " ", my_species)))
 if (length(qu) > 0)
 {
   orgdb_species = qu[[1]]
@@ -29,7 +27,7 @@ if (length(qu) > 0)
 }
 
 ## search for a suitable MeSHDb
-qu = query(ah_species, c("MeSHDb", my_species))
+qu = query(ah_species, c("MeSHDb", sub("_", " ", my_species)))
 if (length(qu) > 0)
 {
   meshdb_species = MeSHDbi::MeSHDb(qu[[1]])
@@ -97,12 +95,12 @@ makeReadable = function(geneIDs, orig_keytype)
 print(" --- Gene Ontology overrepresentation analysis")
 
 GO_HI = enrichGO(gene     = entrezIDs_HI$ENTREZID,
-                OrgDb         = orgdb_species,
-                keyType       = 'ENTREZID',
-                ont           = "CC",
-                pAdjustMethod = "BH",
-                pvalueCutoff  = 0.01,
-                qvalueCutoff  = 0.05)
+                 OrgDb         = orgdb_species,
+                 keyType       = 'ENTREZID',
+                 ont           = "CC",
+                 pAdjustMethod = "BH",
+                 pvalueCutoff  = 0.01,
+                 qvalueCutoff  = 0.05)
 name_HI = paste(workflow_name,"GO_HI",sep="_")
 file_name_HI = paste(name_HI, ".txt")
 figure_name_HI = paste(name_HI, ".png")
@@ -111,12 +109,12 @@ write.table(GO_HI, file = file_name_HI, sep = "\t", quote = F, row.names = F, na
 head(summary(GO_HI), n=3)
 head(GO_HI)
 GO_ME = enrichGO(gene = entrezIDs_ME$ENTREZID,
-               keyType       = "ENTREZID",
-               OrgDb         = orgdb_species,
-               ont           = "ALL",
-               pAdjustMethod = "BH",
-               pvalueCutoff  = 0.01,
-               qvalueCutoff  = 0.05)
+                 keyType       = "ENTREZID",
+                 OrgDb         = orgdb_species,
+                 ont           = "ALL",
+                 pAdjustMethod = "BH",
+                 pvalueCutoff  = 0.01,
+                 qvalueCutoff  = 0.05)
 name_ME = paste(workflow_name,"GO_ME",sep="_")
 file_name_ME = paste(name_ME, ".txt")
 figure_name_ME = paste(name_ME, ".png")
@@ -132,27 +130,27 @@ if (!is.null(GO_HI)) {
   barplot(GO_HI, showCategory=20, title="Enrichment Analysis - representation of gene ontology on HI significant samples") 
   dev.off()
 } else {
-    print("Failed to return results of Gene Ontology for HI significant!")
+  print("Failed to return results of Gene Ontology for HI significant!")
 }
 if (!is.null(GO_ME)) {
   png(figure_name_ME, width = 750, height = 950)
   barplot(GO_ME, showCategory=20, title="Enrichment Analysis - representation of gene ontology on ME significant samples") 
   dev.off()
 } else {
-    print("Failed to return results of Gene Ontology for ME significant!")
+  print("Failed to return results of Gene Ontology for ME significant!")
 }
 
 
 
 # search the organism for kegg analysis
-kegg_organism = search_kegg_organism(my_species)$kegg_code
+kegg_organism = search_kegg_organism(sub("_", " ", my_species))$kegg_code
 
 print(" --- KEGG pathway over-representation analysis on the extracted genes")
 
 # KEGG pathway over-representation analysis on the extracted genes
 kegg_HI = enrichKEGG(gene           = sgnfGenes_HI,
-                        organism     = kegg_organism,
-                        pvalueCutoff = 0.05)
+                     organism     = kegg_organism,
+                     pvalueCutoff = 0.05)
 
 kegg_name_HI = paste(workflow_name,"kegg_HI",sep="_")
 file_name_kegg_HI = paste(kegg_name_HI, ".txt")
@@ -162,8 +160,8 @@ write.table(kegg_HI, file = file_name_kegg_HI, sep = "\t", quote = F, row.names 
 head(kegg_HI)
 
 kegg_ME = enrichKEGG(gene                = sgnfGenes_ME,
-                            organism     = kegg_organism,
-                            pvalueCutoff = 0.05)
+                     organism     = kegg_organism,
+                     pvalueCutoff = 0.05)
 kegg_ME_readable = kegg_ME
 kegg_ME_readable@result$geneID = makeReadable(kegg_ME@result$geneID, orig_keytype = "ORF")
 kegg_name_ME = paste(workflow_name,"kegg_ME",sep="_")
@@ -212,11 +210,11 @@ head(entrez_HI)
 # typeof(entrez_HI)
 
 x_HI = enrichDO(gene          = entrez_HI,
-                 ont           = "DO",
-                 pvalueCutoff  = 0.05,
-                 pAdjustMethod = "BH",
-                 qvalueCutoff  = 0.05,
-                 readable      = FALSE)
+                ont           = "DO",
+                pvalueCutoff  = 0.05,
+                pAdjustMethod = "BH",
+                qvalueCutoff  = 0.05,
+                readable      = FALSE)
 
 x_name_HI = paste(workflow_name,"disease_ont_HI",sep="_")
 file_name_x_HI = paste(x_name_HI, ".txt")
@@ -226,11 +224,11 @@ write.table(x_HI, file = file_name_x_HI, sep = "\t", quote = F, row.names = F, n
 head(x_HI)
 
 x_ME = enrichDO(gene                = entrezIDs_ME$ENTREZID,
-                 ont           = "DO",
-                 pvalueCutoff  = 0.05,
-                 pAdjustMethod = "BH",
-                 qvalueCutoff  = 0.05,
-                 readable      = FALSE)
+                ont           = "DO",
+                pvalueCutoff  = 0.05,
+                pAdjustMethod = "BH",
+                qvalueCutoff  = 0.05,
+                readable      = FALSE)
 x_name_ME = paste(workflow_name,"disease_ont_ME",sep="_")
 file_name_x_ME = paste0(x_name_ME, ".txt")
 figure_name_x_ME = paste0(x_name_ME, ".png")
@@ -260,28 +258,28 @@ if (!is.null(x_ME)) {
 
 
 # MeSH enrichment analysis
-qu = query(ah_species, c("MeSHDb", sub("_", " ", my_species)))
+qu = query(ah_species, c("MeSHDb", sub("_", " ", sub("_", " ", my_species))))
 if (length(qu) > 0) {
   print(" --- MeSH enrichment analysis...")
-
+  
   file_qu = qu[[1]]
   db = MeSHDbi::MeSHDb(file_qu)
-
+  
   # de_HI = names(sgnfGenes_HI)[1:100]
   # mesh_HI = enrichMeSH(de_HI, MeSHDb = db, database='gendoo', category = 'C')
   mesh_HI = tryCatch(enrichMeSH(na.omit(entrezIDs_HI$ENTREZID), MeSHDb = meshdb_species, database='gendoo', category = 'C'),
-           error=function(cond) {
-             return(NA)
-           },
-           warning=function(cond) {
-             # return(NA)
-           })
- 
+                     error=function(cond) {
+                       return(NA)
+                     },
+                     warning=function(cond) {
+                       # return(NA)
+                     })
+  
   mesh_name_HI = paste(workflow_name,"mesh_HI",sep="_")
   file_name_mesh_HI = paste0(mesh_name_HI, ".txt")
   figure_name_mesh_HI = paste0(mesh_name_HI, ".png")
   write.table(mesh_HI, file = file_name_mesh_HI, sep = "\t", quote = F, row.names = F, na = "")
-
+  
   # de_ME = names(sgnfGenes_ME)[1:100]
   # mesh_ME = enrichMeSH(de_ME, MeSHDb = db, database='gendoo', category = 'C')
   mesh_ME = tryCatch(enrichMeSH(na.omit(entrezIDs_ME$ENTREZID), MeSHDb = meshdb_species, database='gendoo', category = 'C'),
@@ -291,27 +289,27 @@ if (length(qu) > 0) {
                      warning=function(cond) {
                        # return(NA)
                      })
-
+  
   mesh_name_ME = paste(workflow_name,"mesh_ME",sep="_")
   file_name_mesh_ME = paste0(mesh_name_ME, ".txt")
   figure_name_mesh_ME = paste0(mesh_name_ME, ".png")
   write.table(mesh_ME, file = file_name_mesh_ME, sep = "\t", quote = F, row.names = F, na = "")
-
+  
   # plot the results
   if (!is.null(de_HI)) {
     png(figure_name_mesh_HI, width = 600, height = 650)
     barplot(mesh_HI)
     dev.off()
   } else {
-      print("Failed to return MeSH enrichment analysis results for the given list of HI significant significant genes!")
+    print("Failed to return MeSH enrichment analysis results for the given list of HI significant significant genes!")
   }
-
+  
   if (!is.null(de_ME)) {
     png(figure_name_mesh_ME, width = 600, height = 650)
     barplot(mesh_ME)
     dev.off()
   } else {
-      print("Failed to return MeSH enrichment analysis results for the given list of ME significant significant genes!")
+    print("Failed to return MeSH enrichment analysis results for the given list of ME significant significant genes!")
   }
 } else
 {
