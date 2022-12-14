2764bfea36d365f3cc78e7b520b13ef7b0e1b120
amizeranschi <alex.mizeranschi@gmail.com>
2022-12-14 22:18:00 +0200

    Update structural_variants_annotation.sh
    Single extra env

--- bcbio_wrapper_scripts/downstreamAnalysis/structural_variants_annotation.sh
+++ bcbio_wrapper_scripts/downstreamAnalysis/structural_variants_annotation.sh
@@ -85,7 +85,7 @@ if (( "$file_size" > "0" )); then
         # vep --fork 4 --tab --pick --no_stats --biotype --check_existing --distance 1000000 --symbol --species ${bcbio_vep_species} --cache --dir_cache ${genome_dir}/vep --input_file ${vcf_file_name}.vcf --output_file ${vcf_file_name}-vep.table --force_overwrite --offline --max_sv_size 1000000000
         
         echo " --- [$(date +"%F %R")] Running VEP with the official cache for the species: ${bcbio_vep_species}, version ${bcbio_ensembl_ver}"
-        ${bcbio_install_path}/extra3/bin/vep --fork 4 --tab --pick --biotype --check_existing --distance 1000000 --symbol --species $(echo "$bcbio_vep_species" | awk '{print tolower($0)}') --cache --dir_cache ${genome_dir}/vep --input_file ${vcf_file_name}.vcf --output_file ${vcf_file_name}-vep.table --force_overwrite --stats_file ${vcf_file_name}-vep.stats --stats_text --offline --max_sv_size 1000000000
+        ${bcbio_install_path}/extra/envs/py3/bin/vep --fork 4 --tab --pick --biotype --check_existing --distance 1000000 --symbol --species $(echo "$bcbio_vep_species" | awk '{print tolower($0)}') --cache --dir_cache ${genome_dir}/vep --input_file ${vcf_file_name}.vcf --output_file ${vcf_file_name}-vep.table --force_overwrite --stats_file ${vcf_file_name}-vep.stats --stats_text --offline --max_sv_size 1000000000
         echo " --- [$(date +"%F %R")] Variant consequences were written to the file: ${vcf_file_name}-vep.table"
         
     else
@@ -113,7 +113,7 @@ if (( "$file_size" > "0" )); then
         # vep --fork 4 --tab --pick --no_stats --biotype --check_existing --distance 1000000 --symbol --fasta ${genome_dir}/seq/${bcbio_genome}.fa --custom ${genome_dir}/rnaseq/ref-transcripts.gtf,ref-transcripts,gtf,overlap,0 --input_file ${vcf_file_name}-no-BND.vcf --output_file ${vcf_file_name}-no-BND-vep.table --force_overwrite --offline --max_sv_size 1000000000
         
         echo " --- [$(date +"%F %R")] Running VEP with custom annotations from the GTF file: ${genome_dir}/rnaseq/ref-transcripts.gtf"
-        ${bcbio_install_path}/extra3/bin/vep --fork 4 --tab --pick --biotype --check_existing --distance 1000000 --symbol --fasta ${genome_dir}/seq/${bcbio_genome}.fa --custom ${genome_dir}/rnaseq/ref-transcripts.gtf,ref-transcripts,gtf,overlap,0 --input_file ${vcf_file_name}-no-BND.vcf --output_file ${vcf_file_name}-vep.table --force_overwrite --stats_file ${vcf_file_name}-vep.stats --stats_text --offline --max_sv_size 1000000000
+        ${bcbio_install_path}/extra/envs/py3/bin/vep --fork 4 --tab --pick --biotype --check_existing --distance 1000000 --symbol --fasta ${genome_dir}/seq/${bcbio_genome}.fa --custom ${genome_dir}/rnaseq/ref-transcripts.gtf,ref-transcripts,gtf,overlap,0 --input_file ${vcf_file_name}-no-BND.vcf --output_file ${vcf_file_name}-vep.table --force_overwrite --stats_file ${vcf_file_name}-vep.stats --stats_text --offline --max_sv_size 1000000000
         echo " --- [$(date +"%F %R")] Variant consequences were written to the file: ${vcf_file_name}-vep.table"
     fi
     ## extract relevant columns (GT and PR,SR) from a VCF file into tab-separated files
