ad526eb39c53e1765526e246d9397457866f7090
amizeranschi <alex.mizeranschi@gmail.com>
2022-12-20 12:27:11 +0200

    Update setup_python_envs.sh
    Rename extra conda and mamba soft links

--- bcbio_wrapper_scripts/environment/setup_python_envs.sh
+++ bcbio_wrapper_scripts/environment/setup_python_envs.sh
@@ -6,25 +6,25 @@ if [[ ! -d ${bcbio_install_path}/extra ]]; then
    echo "--- [$(date +"%F %R")] Setting up custom Python3 and Python2 environments for utility packages"
    
    bash ${path_to_scripts}/Miniconda3-*.sh -b -p ${bcbio_install_path}/extra
-   ln -s ${bcbio_install_path}/extra/bin/conda ${bcbio_install_path}/extra/bin/conda_extra
+   ln -s ${bcbio_install_path}/extra/bin/conda ${bcbio_install_path}/extra/bin/extra_conda
    
-   ${bcbio_install_path}/extra/bin/conda_extra install --yes -c conda-forge -c bioconda "mamba>=0.27"
+   ${bcbio_install_path}/extra/bin/extra_conda install --yes -c conda-forge -c bioconda "mamba>=0.27"
    
-   ln -s ${bcbio_install_path}/extra/bin/mamba ${bcbio_install_path}/extra/bin/mamba_extra
+   ln -s ${bcbio_install_path}/extra/bin/mamba ${bcbio_install_path}/extra/bin/extra_mamba
    
    ## create python2 and python3 environments
    
-   ${bcbio_install_path}/extra/bin/mamba_extra create --name py3 python=3.9 -y
-   ${bcbio_install_path}/extra/bin/mamba_extra create --name py2 python=2.7.18 -y
+   ${bcbio_install_path}/extra/bin/extra_mamba create --name py3 python=3.9 -y
+   ${bcbio_install_path}/extra/bin/extra_mamba create --name py2 python=2.7.18 -y
    
    echo "--- [$(date +"%F %R")] Installing packages in the Python2 environment"
    
-   ${bcbio_install_path}/extra/bin/mamba_extra install --name py2 --yes -c conda-forge -c bioconda faststructure pathoscope
+   ${bcbio_install_path}/extra/bin/extra_mamba install --name py2 --yes -c conda-forge -c bioconda faststructure pathoscope
    ${bcbio_install_path}/extra/envs/py2/bin/pip install bcbio-monitor pytz python-dateutil
    
    echo "--- [$(date +"%F %R")] Installing packages in the Python3 environment"
    
-   ${bcbio_install_path}/extra/bin/mamba_extra install --name py3 --yes -c conda-forge -c bioconda kraken2 blast spades masurca quast \
+   ${bcbio_install_path}/extra/bin/extra_mamba install --name py3 --yes -c conda-forge -c bioconda kraken2 blast spades masurca quast \
    gxx_linux-64 wget git bedops vcftools pyyaml perl-yaml \
    perl-encode-locale "sra-tools>=2.11" perl-net-ssleay entrez-direct tassel beagle gcta "gdal>=2.0.1" plink "bcftools>=1.15" haploview \
    "ensembl-vep>=105" "perl-bioperl>=1.7.2" "r-base>=4.2" r-essentials r-xml r-knitr r-markdown r-rmarkdown r-shiny r-rcpp r-devtools r-reticulate \
