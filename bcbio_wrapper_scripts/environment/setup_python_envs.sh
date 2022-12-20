f5b13dacd04039ae49c498e730fb11df22186533
amizeranschi <alex.mizeranschi@gmail.com>
2022-12-20 19:49:47 +0200

    Update setup_python_envs.sh
    Add missing package bioconductor-ahmeshdbs

--- bcbio_wrapper_scripts/environment/setup_python_envs.sh
+++ bcbio_wrapper_scripts/environment/setup_python_envs.sh
@@ -28,8 +28,8 @@ if [[ ! -d ${bcbio_install_path}/extra ]]; then
    gxx_linux-64 wget git bedops vcftools pyyaml perl-yaml \
    perl-encode-locale "sra-tools>=2.11" perl-net-ssleay entrez-direct tassel beagle gcta "gdal>=2.0.1" plink "bcftools>=1.15" haploview \
    "ensembl-vep>=105" "perl-bioperl>=1.7.2" "r-base>=4.2" r-essentials r-xml r-knitr r-markdown r-rmarkdown r-shiny r-rcpp r-devtools r-reticulate \
-   perl-bio-tools-run-alignment-tcoffee r-terra r-rgdal r-ragg r-ade4 r-aghmatrix bioconductor-annotationdbi bioconductor-annotationhub r-bedr r-bglr r-candisc \
-   r-car bioconductor-chipseeker bioconductor-clusterprofiler r-cmplot r-corrgram r-corrplot r-data.table bioconductor-deseq2 bioconductor-do.db \
+   perl-bio-tools-run-alignment-tcoffee r-terra r-rgdal r-ragg r-ade4 r-aghmatrix bioconductor-ahmeshdbs bioconductor-annotationdbi bioconductor-annotationhub r-bedr \
+   r-bglr r-candisc r-car bioconductor-chipseeker bioconductor-clusterprofiler r-cmplot r-corrgram r-corrplot r-data.table bioconductor-deseq2 bioconductor-do.db \
    r-doparallel bioconductor-dose r-dplyr r-dt r-emmeans bioconductor-enrichplot bioconductor-ensembldb bioconductor-gdsfmt bioconductor-geneoverlap \
    bioconductor-genesis r-ggally r-ggplot2 r-ggrepel r-ggroups r-ggupset bioconductor-gosemsim r-gparotation r-gtools r-hierfstat r-lsmeans bioconductor-meshdbi \
    bioconductor-meshes r-misctools r-multcomp r-nnet r-orthopolynom r-outliers bioconductor-pathview bioconductor-pcamethods r-pheatmap r-plotly r-psych r-qqman \
