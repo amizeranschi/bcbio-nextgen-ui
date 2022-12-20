a1ca9c7db7a199fd3375c66b1c2457a05c70cf08
amizeranschi <alex.mizeranschi@gmail.com>
2022-12-20 14:36:26 +0200

    Update setup_python_envs.sh
    Add new packages for ChIP-seq downstream analysis

--- bcbio_wrapper_scripts/environment/setup_python_envs.sh
+++ bcbio_wrapper_scripts/environment/setup_python_envs.sh
@@ -28,12 +28,12 @@ if [[ ! -d ${bcbio_install_path}/extra ]]; then
    gxx_linux-64 wget git bedops vcftools pyyaml perl-yaml \
    perl-encode-locale "sra-tools>=2.11" perl-net-ssleay entrez-direct tassel beagle gcta "gdal>=2.0.1" plink "bcftools>=1.15" haploview \
    "ensembl-vep>=105" "perl-bioperl>=1.7.2" "r-base>=4.2" r-essentials r-xml r-knitr r-markdown r-rmarkdown r-shiny r-rcpp r-devtools r-reticulate \
-   perl-bio-tools-run-alignment-tcoffee r-terra r-rgdal r-ragg r-ade4 r-aghmatrix bioconductor-annotationdbi bioconductor-annotationhub r-bglr r-candisc \
+   perl-bio-tools-run-alignment-tcoffee r-terra r-rgdal r-ragg r-ade4 r-aghmatrix bioconductor-annotationdbi bioconductor-annotationhub r-bedr r-bglr r-candisc \
    r-car bioconductor-chipseeker bioconductor-clusterprofiler r-cmplot r-corrgram r-corrplot r-data.table bioconductor-deseq2 bioconductor-do.db \
-   r-doparallel bioconductor-dose r-dplyr r-dt r-emmeans bioconductor-enrichplot bioconductor-ensembldb bioconductor-gdsfmt \
-   bioconductor-genesis r-ggally r-ggplot2 r-ggrepel r-ggroups bioconductor-gosemsim r-gparotation r-gtools r-hierfstat r-lsmeans bioconductor-meshdbi \
-   bioconductor-meshes r-misctools r-multcomp r-nnet r-orthopolynom r-outliers bioconductor-pcamethods r-pheatmap r-plotly r-psych r-qqman \
-   bioconductor-qvalue r-raster r-rcolorbrewer bioconductor-reactomepa r-readxl r-reshape2 r-rgdal r-rlist r-rrblup r-rstatix \
+   r-doparallel bioconductor-dose r-dplyr r-dt r-emmeans bioconductor-enrichplot bioconductor-ensembldb bioconductor-gdsfmt bioconductor-geneoverlap \
+   bioconductor-genesis r-ggally r-ggplot2 r-ggrepel r-ggroups r-ggupset bioconductor-gosemsim r-gparotation r-gtools r-hierfstat r-lsmeans bioconductor-meshdbi \
+   bioconductor-meshes r-misctools r-multcomp r-nnet r-orthopolynom r-outliers bioconductor-pathview bioconductor-pcamethods r-pheatmap r-plotly r-psych r-qqman \
+   bioconductor-qvalue r-raster r-rcolorbrewer bioconductor-reactomepa r-readxl r-reshape2 r-rgdal r-rlist r-rrblup r-rstatix bioconductor-sbgnview \
    bioconductor-snprelate bioconductor-snpstats r-sp r-stringr r-tidyr r-tidyverse bioconductor-variantannotation r-vcfr r-vegan r-writexl
    
    ## install the Encode::Locale perl module
