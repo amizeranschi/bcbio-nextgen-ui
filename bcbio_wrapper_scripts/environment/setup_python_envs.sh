9c5e3f812d6d7152233d9d0e3883842f18a693b3
amizeranschi <alex.mizeranschi@gmail.com>
2023-10-25 14:29:25 +0300

    Revert "Update setup_python_envs.sh"
    This reverts commit 13c158c74cefe528d743d76f0df055d0a0963f6a.

--- bcbio_wrapper_scripts/environment/setup_python_envs.sh
+++ bcbio_wrapper_scripts/environment/setup_python_envs.sh
@@ -27,13 +27,13 @@ if [[ ! -d ${bcbio_install_path:?}/extra ]]; then
    ${bcbio_install_path:?}/extra/bin/extra_mamba install --name py3 --yes -c conda-forge -c bioconda kraken2 krakenuniq blast spades masurca quast \
    flye shasta repeatmodeler repeatmasker gxx_linux-64 wget git bedops vcftools pyyaml perl-yaml tectonic gnuplot "openjdk>=12" r-rjava \
    perl-encode-locale "sra-tools>=2.11" perl-net-ssleay entrez-direct tassel beagle gcta "gdal>=2.0.1" plink "bcftools>=1.15" "samtools>=1.15" haploview \
-   "ensembl-vep>=109" "perl-bioperl>=1.7.2" "r-base=4.3.1" r-essentials r-xml r-knitr r-markdown r-rmarkdown r-shiny r-shinyngs r-rcpp r-devtools r-reticulate \
+   "ensembl-vep>=109" "perl-bioperl>=1.7.2" "r-base>=4.2" r-essentials r-xml r-knitr r-markdown r-rmarkdown r-shiny r-shinyngs r-rcpp r-devtools r-reticulate \
    perl-bio-tools-run-alignment-tcoffee r-terra r-rgdal r-ragg r-ade4 r-aghmatrix bioconductor-ahmeshdbs bioconductor-annotationdbi bioconductor-annotationhub r-bedr \
    r-bglr r-candisc r-car bioconductor-chipseeker bioconductor-clusterprofiler r-cmplot r-corrgram r-corrplot r-data.table bioconductor-deseq2 bioconductor-do.db \
    r-doparallel bioconductor-dose r-dplyr r-dt r-emmeans bioconductor-enrichplot bioconductor-ensembldb bioconductor-gdsfmt bioconductor-geneoverlap \
    bioconductor-genesis r-ggally r-ggplot2 r-ggrepel r-ggroups r-ggupset bioconductor-gosemsim r-gparotation r-gtools r-hierfstat r-lsmeans bioconductor-meshdbi \
    bioconductor-meshes r-misctools r-multcomp r-nnet r-orthopolynom r-outliers bioconductor-pathview bioconductor-pcamethods r-pheatmap r-plotly r-psych r-qqman \
-   bioconductor-qvalue r-raster r-rcolorbrewer bioconductor-reactomepa r-readxl r-reshape2 r-rgdal r-rlist r-rrblup r-rstatix \
+   bioconductor-qvalue r-raster r-rcolorbrewer bioconductor-reactomepa r-readxl r-reshape2 r-rgdal r-rlist r-rrblup r-rstatix bioconductor-sbgnview \
    bioconductor-snprelate bioconductor-snpstats r-sp r-stringr r-tidyr r-tidyverse bioconductor-variantannotation r-vcfr r-vegan r-writexl
    
    ## install the Encode::Locale perl module
